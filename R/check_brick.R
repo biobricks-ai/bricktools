f9122c579c1a9a60905d60017c977840c15aa159
James Borden <jmborden@gmail.com>
2022-05-25 17:00:28 -0400

    add brick_ls_works test

--- R/check_brick.R
+++ R/check_brick.R
@@ -1,3 +1,23 @@
+#' Check that biobricks::brick_ls_remote() can be run
+#' on brick
+#' @param brickname the name of the brick check
+#' @export
+#' @importFrom biobricks local_bblib install_brick brick_ls_remote
+#' @return list(valid = boolean, error_msg = string)
+brick_ls_works <- function(brickname) {
+    tryCatch(
+        {
+            local_bblib()
+            install_brick(brickname)
+            brick_ls_remote(brickname)
+            return(list(valid = TRUE))
+        },
+        error = function(e) {
+            return(list(valid = FALSE, error_msg = e$message))
+        }
+    )
+}
+
 #' Check that a file exists and is nonempty
 #' @param filename to check repo
 #' @return list(valid = boolean, error_msg = string)
@@ -13,15 +33,26 @@ file_exists_check <- function(filename) {
 #' @param dir of the brick repo
 #' @return a list of test results
 #' @importFrom purrr map flatten
+#' @importFrom fs path_real
 #' @export
 validate <- function(dir) {
-    c("dvc.yaml", "dvc.lock", ".dvc/config", "README.md") |>
+
+    # check that files exist
+    result <- c("dvc.yaml", "dvc.lock", ".dvc/config", "README.md") |>
         map(~ {
             x <- list()
             x[[.x]] <- file_exists_check(file.path(dir, .x))
             x
         }) |>
         flatten()
+    # check that the brick can list its files
+    result <- append(result, list(brick_ls_works = {
+        dir |>
+            path_real() |>
+            basename() |>
+            brick_ls_works()
+    }))
+    return(result)
 }
 
 #' Check whether or not the brick is valid
@@ -31,12 +62,12 @@ validate <- function(dir) {
 #' In order to do this, this function tests for the presence of the following
 #' files: dvc.yaml, dvc.lock, .dvc/config, and README.md.
 #'
-#' @param dir of the brick repo
+#' @param validate_result a list of validation results returned by validate
 #' @return TRUE if the brick passes all tests and FALSE if it does not.
 #' @importFrom purrr map
 #' @export
-valid <- function(dir) {
-    validate(dir) |>
+valid <- function(validate_result) {
+    validate_result |>
         map(~ .x$valid) |>
         unlist() |>
         all()
@@ -44,12 +75,12 @@ valid <- function(dir) {
 
 #' Return errors associated with a brick
 #'
-#' @param dir of the brick repo
+#' @param validate_result a list of validation results returned by validate
 #' @return list of errors
 #' @importFrom purrr map keep flatten
 #' @export
-errors <- function(dir) {
-    errors <- validate(dir) |>
+errors <- function(validate_result) {
+    errors <- validate_result |>
         purrr::keep(~ isFALSE(.x$valid)) |>
         purrr::map(~ .x$error_msg)
     if (length(errors) == 0) {
@@ -66,8 +97,9 @@ errors <- function(dir) {
 #' @importFrom stringr str_flatten
 #' @export
 check <- function(dir) {
-    if (valid(dir)) {
-		cat("All brick checks passed.\n")
+    validate_result <- validate(dir)
+    if (valid(validate_result)) {
+        cat("All brick checks passed.\n")
         if (interactive()) {
             TRUE
         } else {
@@ -75,7 +107,9 @@ check <- function(dir) {
         }
     } else {
         # print errors
-        errors(dir) |> map(~ { cat(paste0(.x,"\n"))})
+        errors(validate_result) |> map(~ {
+            cat(paste0(.x, "\n"))
+        })
         if (interactive()) {
             FALSE
         } else {
